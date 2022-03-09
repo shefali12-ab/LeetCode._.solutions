2dd4f4acc31278cbfb97e64eb869753483dca4bb
shefali kanojia <56781823+shefali12-ab@users.noreply.github.com>
2022-03-09 19:34:33 +0530

    Update 35-search-insert-position.cpp

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         
-        
-        int low=0;
-        int high=nums.size()-1;
+        //using Binary serach algorithm
+        int low=0;//it points to element at index 0 in vector
+        int high=nums.size()-1;//it points to element at last index in vector
        
         
         while(low<=high)
         {
         int mid=(low+high)/2;
-            
+         // int mid = low +(high-low)/2 it can also be wriiten to avoid overflow   
         if(target==nums[mid]) return mid;
         else if(target<nums[mid]) high=mid-1;
         else  low= mid+1;
@@ -22,3 +22,5 @@ public:
       
     }
 };
+// This question can be done in one line using c++ stls
+//Also it can be done using recursive binary serach method.
