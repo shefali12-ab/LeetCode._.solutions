d254985a7424863a158f50f847b74931946b5fa3
shefali kanojia <56781823+shefali12-ab@users.noreply.github.com>
2022-03-17 09:56:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.65%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int fib(int n) {
-        return fnth(n);
+        map<int,int> m;
+        return fnth(n,m);
     }
     private:
-    int fnth(int n){
+    int fnth(int n,map<int,int> &m){
         if(n==1) return 1;
         if(n==0) return 0;
-        int a=fnth(n-1);
-        int b=fnth(n-2);
+        int currentKey=n;
+        if(m.find(currentKey)!=m.end())
+            return m[currentKey];
+        
+        int a=fnth(n-1,m);
+        int b=fnth(n-2,m);
+        
+        m[currentKey]=a+b;
         return a+b;
     }
 };
